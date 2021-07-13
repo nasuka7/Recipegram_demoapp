b180248f861b1aea54bb8084f65648a6658a0bfc
nasuka7 <n4suka7@gmail.com>
2021-07-13 23:46:45 +0900

    ✨ レシピ周りのページのためのルートの構築

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_025002) do
+ActiveRecord::Schema.define(version: 2021_07_13_114251) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
